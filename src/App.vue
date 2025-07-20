2c39e9229dd95c51f86c7f872c004e230f7d2ce5
Railtonoficial <railtonaraujoofc@gmail.com>
2025-07-20 15:25:59 -0300

    Projeto feito durante estudos

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Banner />
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup>
+import { defineComponent } from 'vue';
+import Banner from './components/Banner.vue';
+
+defineComponent({
+  name: 'App',
+  components: {
+    Banner
+  }
+});
+</script>
